fc321d5a15b74cd5e20b5577f0c26b171254bc8b
Ilia Os <dev.osipov@gmail.com>
2024-02-14 19:50:19 +0400

    chore: move s3 configuration options under s3Options

--- cms/config/env/production/plugins.ts
+++ cms/config/env/production/plugins.ts
@@ -5,15 +5,17 @@ export default ({ env }: TConfigInput) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('DO_SPACE_ACCESS_KEY'),
-        secretAccessKey: env('DO_SPACE_ACCESS_SECRET'),
-        endpoint: env('DO_SPACE_ENDPOINT', 'https://ams3.digitaloceanspaces.com'),
-        region: env('DO_SPACE_REGION', 'ams3'),
-        params: {
-          ACL: env('DO_SPACE_ACL', 'public-read'),
-          signedUrlExpires: env('DO_SPACE_SIGNED_URL_EXPIRES', 15 * 60),
-          Bucket: env('DO_SPACE_BUCKET', 'spencer-web'),
-        },
+        s3Options: {
+          accessKeyId: env('DO_SPACE_ACCESS_KEY'),
+          secretAccessKey: env('DO_SPACE_ACCESS_SECRET'),
+          endpoint: env('DO_SPACE_ENDPOINT', 'https://ams3.digitaloceanspaces.com'),
+          region: env('DO_SPACE_REGION', 'ams3'),
+          params: {
+            ACL: env('DO_SPACE_ACL', 'public-read'),
+            signedUrlExpires: env('DO_SPACE_SIGNED_URL_EXPIRES', 15 * 60),
+            Bucket: env('DO_SPACE_BUCKET', 'spencer-web'),
+          },
+        }
       },
       actionOptions: {
         upload: {},
