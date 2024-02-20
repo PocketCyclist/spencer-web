56059dd3904c2d38af3df37522ce166e26805e86
Alex Kalinin <alessandro.kalinin@gmail.com>
2024-02-20 20:14:05 +0300

    feat: add footer component

---
+++ frontend/src/components/layout/Footer/FooterSubscribe.tsx
@@ -0,0 +1,12 @@
+export const FooterSubscribe = () => (
+  <div>
+    <h5 className="mb-4 font-serif font-bold text-footer-title">
+      Subscribe for newslatters
+    </h5>
+    <input
+      className="w-full h-10 px-3.5 py-1.5 border border-input-border placeholder:text-input-placeholder"
+      placeholder="Enter your email"
+      type="email"
+    />
+  </div>
+)
