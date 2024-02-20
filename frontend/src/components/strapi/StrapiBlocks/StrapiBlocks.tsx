5d396b5f962f230e042c4db6bcac4998799cbf3b
Alex Kalinin <alessandro.kalinin@gmail.com>
2024-02-20 22:55:45 +0300

    feat(frontend): add logo hero component

--- frontend/src/components/strapi/StrapiBlocks/StrapiBlocks.tsx
+++ frontend/src/components/strapi/StrapiBlocks/StrapiBlocks.tsx
@@ -1,3 +1,4 @@
+import { LogoHero } from '@/components/strapi/blocks/LogoHero/LogoHero'
 import { Quote } from '@/components/strapi/blocks/Quote/Quote'
 import { TStrapiBlock } from '@/data/strapi/types/common/blocks'
 
@@ -5,11 +6,15 @@ export const StrapiBlocks = ({ blocks }: { blocks: TStrapiBlock[] }) => {
   return blocks.map((block) => {
     switch (block.__component) {
       case 'block.quote':
-        return (
-          <Quote text={block.text as string} author={block.author as string} />
-        )
+        return <Quote text={block.text} author={block.author} />
+      case 'block.logo-hero':
+        return <LogoHero bgSrc={block.backgroundImage.data.attributes.url} />
       default:
-        return <div>Unsupported block: {block.__component}</div>
+        return (
+          <div>
+            Unsupported block: {(block as { __component: string }).__component}
+          </div>
+        )
     }
   })
 }
