54aa401819043f75e2ad65c94d2b80a572bd5981
Alex Kalinin <alessandro.kalinin@gmail.com>
2024-02-21 15:52:41 +0300

    feat(frontend): add video dialog and play button component

---
+++ frontend/src/components/ui/PlayButton/PlayButton.tsx
@@ -0,0 +1,19 @@
+import { HTMLAttributes } from 'react'
+
+import { PlayIcon } from '@/icons'
+import { cn } from '@/lib/cn'
+
+type PlayButtonProps = HTMLAttributes<HTMLButtonElement>
+
+export const PlayButton = ({ className, ...props }: PlayButtonProps) => (
+  <button
+    className={cn(
+      'size-[88px] inline-flex justify-center items-center text-red xl:size-[237px]',
+      className,
+    )}
+    type="button"
+    {...props}
+  >
+    <PlayIcon className="size-full" viewBox="0 0 238 237" />
+  </button>
+)
