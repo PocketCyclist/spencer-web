3f4fab251c4f4fd96d55b06be4fb8de4c0131a18
Alex Kalinin <alessandro.kalinin@gmail.com>
2024-02-22 16:20:03 +0300

    feat: add discography albums

--- frontend/src/data/strapi/types/albums.d.ts
+++ frontend/src/data/strapi/types/albums.d.ts
@@ -3,10 +3,10 @@ import {
   TStrapiImageField,
 } from '@/data/strapi/types/common/api'
 
-export type TStrapiAlbum = TStrapiEntity<{
+export type TStrapiAlbum = {
   title: string
   description: string
   videoUrl: string
   buyUrl: string
   cover: TStrapiImageField
-}>
+}
