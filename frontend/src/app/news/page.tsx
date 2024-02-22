c53532acc80ddca0ccb7b5bc133bc21160bd6559
Ilia Os <dev.osipov@gmail.com>
2024-02-22 20:43:25 +0400

    chore(frontend): improve news page data

--- frontend/src/app/news/page.tsx
+++ frontend/src/app/news/page.tsx
@@ -6,9 +6,11 @@ import {
 import { TStrapiNewsPage, TStrapiPost } from '@/data/strapi/types/posts'
 
 const News = async () => {
-  const pageData =
-    await strapiGet<TStrapiSingleResponse<TStrapiNewsPage>>('news-page')
-  const posts = await strapiGet<TStrapiListResponse<TStrapiPost>>('posts')
+  const [pageData, posts] = await Promise.all([
+    strapiGet<TStrapiSingleResponse<TStrapiNewsPage>>('news-page'),
+    strapiGet<TStrapiListResponse<TStrapiPost>>('posts'),
+  ])
+
   console.log(pageData, posts)
 
   return (
@@ -16,9 +18,9 @@ const News = async () => {
       <div className="my-4 bg-cyan-50">
         <pre>{JSON.stringify(pageData, null, 2)}</pre>
       </div>
-      {posts.map((post) => (
-        <div key={post.id} className="my-4 bg-amber-50">
-          <pre>{JSON.stringify(post, null, 2)}</pre>
+      {posts.map((event) => (
+        <div key={event.id} className="my-4 bg-amber-50">
+          <pre>{JSON.stringify(event, null, 2)}</pre>
         </div>
       ))}
     </>
