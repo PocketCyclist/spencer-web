a496af7381b5922ab7b3b364b9239597299967ff
Ilia Os <dev.osipov@gmail.com>
2024-02-22 20:41:51 +0400

    chore(frontend): improve projects page data

--- frontend/src/app/projects/page.tsx
+++ frontend/src/app/projects/page.tsx
@@ -9,16 +9,10 @@ import {
 } from '@/data/strapi/types/projects'
 
 const Projects = async () => {
-  const pageData =
-    await strapiGet<TStrapiSingleResponse<TStrapiProjectsPage>>('projects-page')
-  const projects = await strapiGet<TStrapiListResponse<TStrapiProject>>(
-    'projects',
-    {
-      query: {
-        populate: '*', // TODO: deep is not working for projects resource?
-      },
-    },
-  )
+  const [pageData, projects] = await Promise.all([
+    strapiGet<TStrapiSingleResponse<TStrapiProjectsPage>>('projects-page'),
+    strapiGet<TStrapiListResponse<TStrapiProject>>('projects'),
+  ])
   console.log(pageData, projects)
 
   return (
