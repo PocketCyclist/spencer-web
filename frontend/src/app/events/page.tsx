3e973693076d8f94feb7ae1f5d80b21fb476f030
Ilia Os <dev.osipov@gmail.com>
2024-02-22 20:42:29 +0400

    chore(frontend): improve events page data

--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -4,19 +4,45 @@ import {
   TStrapiSingleResponse,
 } from '@/data/strapi/types/common/api'
 import { TStrapiEvent, TStrapiEventsPage } from '@/data/strapi/types/events'
+import { getTodayDate } from '@/data/strapi/utils/date'
 
 const Events = async () => {
-  const pageData =
-    await strapiGet<TStrapiSingleResponse<TStrapiEventsPage>>('events-page')
-  const events = await strapiGet<TStrapiListResponse<TStrapiEvent>>('events')
-  console.log(pageData, events)
+  const today = getTodayDate()
+  const [pageData, futureEvents, pastEvents] = await Promise.all([
+    strapiGet<TStrapiSingleResponse<TStrapiEventsPage>>('events-page'),
+    strapiGet<TStrapiListResponse<TStrapiEvent>>('events', {
+      deepPopulate: true,
+      query: {
+        filters: {
+          date: { $gte: today },
+        },
+      },
+    }),
+    strapiGet<TStrapiListResponse<TStrapiEvent>>('events', {
+      deepPopulate: true,
+      query: {
+        filters: {
+          date: { $lt: today },
+        },
+      },
+    }),
+  ])
+  console.log({ pageData, futureEvents, pastEvents })
 
   return (
     <>
       <div className="my-4 bg-cyan-50">
         <pre>{JSON.stringify(pageData, null, 2)}</pre>
       </div>
-      {events.map((event) => (
+      <div>future events:</div>
+      {futureEvents.map((event) => (
+        <div key={event.id} className="my-4 bg-amber-50">
+          <pre>{JSON.stringify(event, null, 2)}</pre>
+        </div>
+      ))}
+      <hr />
+      <div>past events:</div>
+      {pastEvents.map((event) => (
         <div key={event.id} className="my-4 bg-amber-50">
           <pre>{JSON.stringify(event, null, 2)}</pre>
         </div>
