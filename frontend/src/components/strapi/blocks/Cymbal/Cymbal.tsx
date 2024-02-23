485dad72dcd3246eea6498801bf1e677c769ea7a
Alex Kalinin <alessandro.kalinin@gmail.com>
2024-02-23 12:34:47 +0300

    chore(frontend): show cymbal on 2xl screen and reduce its size

--- frontend/src/components/strapi/blocks/Cymbal/Cymbal.tsx
+++ frontend/src/components/strapi/blocks/Cymbal/Cymbal.tsx
@@ -11,11 +11,11 @@ export const Cymbal = ({ right }: CymbalProps) => {
 
   return (
     <div
-      className="hidden relative -z-[1] select-none 3xl:flex"
+      className="hidden relative -z-[1] select-none 2xl:flex"
       role="presentation"
     >
       <Image
-        className="absolute top-1/2 right-0 -translate-y-1/2"
+        className="rem:w-[340px] absolute top-1/2 right-0 -translate-y-1/2 3xl:rem:w-[513px]"
         alt="Handpan decoration"
         src="/images/handpan-right.png"
         width={513}
