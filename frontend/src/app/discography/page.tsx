a043ca2393fcd7ffbbe22201aea44a6cfb749c66
Ilia Os <dev.osipov@gmail.com>
2024-02-26 23:42:50 +0400

    feat(frontend): change discography page

--- frontend/src/app/discography/page.tsx
+++ frontend/src/app/discography/page.tsx
@@ -14,6 +14,8 @@ import {
 import { Metadata, ResolvingMetadata } from 'next'
 import { TStrapiEventsPage } from '@/data/strapi/types/events'
 import { notFound } from 'next/navigation'
+import { CartIcon, VideoIcon } from '@/icons'
+import { VideoDialog } from '@/components/common/VideoDialog/VideoDialog'
 
 const Discography = async () => {
   const [pageData, albums] = await Promise.all([
@@ -43,7 +45,7 @@ const Discography = async () => {
             className="max-w-[95%] pl-4 flex flex-col flex-shrink-0 snap-start sm:[&:nth-child(2)]:pl-[calc((100vw-(640px-2rem*2))/2)] md:pl-10 md:[&:nth-child(2)]:pl-[calc((100vw-(768px-2rem*2))/2)] xl:rem:pl-[100px] lg:[&:nth-child(2)]:pl-[calc((100vw-(1024px-2rem*2))/2)] xl:[&:nth-child(2)]:pl-[calc((100vw-(1280px-4.125rem*2))/2)]"
           >
             <article className="rem:max-w-[580px] group flex flex-col flex-1 bg-[rgb(217_217_217/100%)]">
-              <div className="rem:h-[318px] relative">
+              <div className="rem:h-[318px] relative min-w-[75vw] md:min-w-[30vw] z-0">
                 {item.attributes.cover && (
                   <Image
                     alt={item.attributes.cover.data.attributes.alternativeText}
@@ -54,33 +56,56 @@ const Discography = async () => {
                   />
                 )}
                 <div className="absolute inset-0 opacity-0 bg-black/40 transition-opacity after:absolute after:inset-0 after:bg-red/50 group-hover:opacity-100" />
-              </div>
-              <div className="p-6 flex flex-col flex-1">
-                <h5 className="mb-4 font-serif rem:text-[36px] leading-none">
+                <h5 className="absolute left-0 right-16 bottom-0 mt-auto p-4 md:p-6 font-serif max-w-[362px] rem:text-[36px] leading-none z-10 text-white font-[48px] transition-opacity opacity-0 group-hover:opacity-100">
                   {item.attributes.title}
                 </h5>
-                <p>{item.attributes.description}</p>
-                <div className="mt-auto pt-9 flex flex-col gap-y-4 sm:flex-row sm:gap-y-0 sm:gap-x-4">
-                  <Button asChild className="sm:flex-1" variant="primary">
-                    <Link
-                      href={item.attributes.buyUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Buy
-                    </Link>
-                  </Button>
-                  <Button asChild className="sm:flex-1" variant="secondary">
-                    <Link
-                      href={item.attributes.videoUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Watch
-                    </Link>
-                  </Button>
-                </div>
+                {item.attributes.buyUrl && (
+                  <Link
+                    href={item.attributes.buyUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="absolute top-4 right-4"
+                  >
+                    <VideoIcon className="transition-opacity opacity-0 group-hover:opacity-100" />
+                  </Link>
+                )}
+                {item.attributes.videoUrl && (
+                  <VideoDialog
+                    src={item.attributes.videoUrl}
+                    trigger={
+                      <button className="absolute bottom-4 right-4">
+                        <CartIcon className="transition-opacity opacity-0 group-hover:opacity-100" />
+                      </button>
+                    }
+                  />
+                )}
               </div>
+              {/*<div className="p-6 flex flex-col flex-1 min-w-[75vw] md:min-w-[30vw]">*/}
+              {/*  <h5 className="mb-4 font-serif rem:text-[36px] leading-none">*/}
+              {/*    {item.attributes.title}*/}
+              {/*  </h5>*/}
+              {/*  <p>{item.attributes.description}</p>*/}
+              {/*  <div className="mt-auto pt-9 flex flex-col gap-y-4 sm:flex-row sm:gap-y-0 sm:gap-x-4">*/}
+              {/*    <Button asChild className="sm:flex-1" variant="primary">*/}
+              {/*      <Link*/}
+              {/*        href={item.attributes.buyUrl}*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        Buy*/}
+              {/*      </Link>*/}
+              {/*    </Button>*/}
+              {/*    <Button asChild className="sm:flex-1" variant="secondary">*/}
+              {/*      <Link*/}
+              {/*        href={item.attributes.videoUrl}*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        Watch*/}
+              {/*      </Link>*/}
+              {/*    </Button>*/}
+              {/*  </div>*/}
+              {/*</div>*/}
             </article>
           </div>
         ))}
