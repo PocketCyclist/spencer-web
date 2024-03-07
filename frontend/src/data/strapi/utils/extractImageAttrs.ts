9b23e8ba54a3108206f2dea4630d456ba88f1c90
Ilia Os <dev.osipov@gmail.com>
2024-03-07 18:25:47 +0400

    fix(frontend): make extractImageAttrs fail-safe

--- frontend/src/data/strapi/utils/extractImageAttrs.ts
+++ frontend/src/data/strapi/utils/extractImageAttrs.ts
@@ -1,6 +1,7 @@
 import { TStrapiImageField } from '@/data/strapi/types/common/api'
 
 export const extractImageAttrs = (field: TStrapiImageField) => {
+  if (!field.data) return { alt: undefined, src: undefined }
   const attrs = field.data.attributes
 
   return {
