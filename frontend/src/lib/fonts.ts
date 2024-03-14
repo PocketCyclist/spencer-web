e00241361939f11c4937e890b0139d2e773e4dfb
Ilia Os <dev.osipov@gmail.com>
2024-03-14 16:04:47 +0400

    chore(frontend): do not load italic fonts

--- frontend/src/lib/fonts.ts
+++ frontend/src/lib/fonts.ts
@@ -2,7 +2,7 @@ import { Mulish, Old_Standard_TT } from 'next/font/google'
 
 export const oldStandardTT = Old_Standard_TT({
   weight: ['400', '700'],
-  style: ['normal', 'italic'],
+  style: ['normal'],
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-old-standard-tt',
@@ -10,7 +10,7 @@ export const oldStandardTT = Old_Standard_TT({
 
 export const mulish = Mulish({
   weight: ['400', '700'],
-  style: ['normal', 'italic'],
+  style: ['normal'],
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-mulish',
