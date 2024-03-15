0de5c14d9cc9b11a5ddfa0dfaad96d086e7016c8
Ilia Os <dev.osipov@gmail.com>
2024-03-15 20:23:14 +0400

    feat(frontend): load resources in defaultLocale if not found in target

--- frontend/src/data/strapi/common.ts
+++ frontend/src/data/strapi/common.ts
@@ -1,9 +1,11 @@
 import qs from 'qs'
 import { getLocale } from 'next-intl/server'
+import { defaultLocale } from '@/navigation'
 
 export type TStrapiGetParams = {
   query?: Record<string, any>
   deepPopulate?: boolean
+  /** @deprecated will be default=true*/
   localized?: boolean
 }
 
@@ -16,20 +18,14 @@ export type TStrapiResource = string
 
 export const strapiGet = async <T extends { data: any }>(
   resource: TStrapiResource,
-  { query, deepPopulate, localized }: TStrapiGetParams = {
+  { query, deepPopulate }: TStrapiGetParams = {
     query: {},
     deepPopulate: true,
     localized: false,
   },
 ): Promise<T['data']> => {
-  const url = new URL(buildResourceUrl(resource))
-  const getQuery: TStrapiQueryParams = {
-    ...(deepPopulate ? { populate: 'deep' } : {}),
-    ...(localized ? { locale: await getLocale() } : {}),
-    ...query,
-  }
-  url.search = qs.stringify(getQuery)
-
+  const locale = await getLocale()
+  const url = buildRequestUrl(resource, { deepPopulate, query, locale })
   // console.log('params', url.toString())
 
   const controller = new AbortController()
@@ -44,28 +40,72 @@ export const strapiGet = async <T extends { data: any }>(
     signal: controller.signal,
     next: { revalidate: 1200 },
   })
+    .finally(() => clearTimeout(timeoutId))
     .then((response) => {
       if (!response.ok) {
-        throw new Error(
-          `Strapi HTTP error! Url: ${url} Status: ${response.status}`,
-        )
+        if (response.status === 404 && locale !== defaultLocale) {
+          const retryUrl = buildRequestUrl(resource, {
+            deepPopulate,
+            query,
+          })
+          const retryController = new AbortController()
+          const retryTimeoutId = setTimeout(
+            () => retryController.abort(),
+            10000,
+          )
+          return fetch(retryUrl, {
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${process.env.STRAPI_TOKEN as string}`,
+              accept: 'application/json',
+            },
+            signal: retryController.signal,
+            next: { revalidate: 1200 },
+          })
+            .finally(() => clearTimeout(retryTimeoutId))
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error(
+                  `Strapi RETRY HTTP error! Url: ${url} Status: ${response.status}`,
+                )
+              }
+
+              return response.json()
+            })
+        } else {
+          throw new Error(
+            `Strapi HTTP error! Url: ${url} Status: ${response.status}`,
+          )
+        }
       }
       return response.json()
     })
-    .then((r) => r.data as T['data'])
+    .then((r) => {
+      return r.data as T['data']
+    })
     .catch((error) => {
       if (error.name === 'AbortError') {
         console.error('Request timed out:', error)
       } else {
-        console.error(
-          'LOOK ME UP FOR DEFAULT LOCALE RETRY Error:',
-          error.message || 'An unknown error occurred',
-        )
+        console.error('Error:', error.message || 'An unknown error occurred')
       }
       throw error
     })
-    .finally(() => clearTimeout(timeoutId))
 }
 
 const buildResourceUrl = (resource: TStrapiResource) =>
   `${process.env.STRAPI_URL}/api/${resource}`
+
+const buildRequestUrl = (
+  resource: TStrapiResource,
+  { deepPopulate, query, locale }: { locale?: string } & TStrapiGetParams,
+) => {
+  const url = new URL(buildResourceUrl(resource))
+  const getQuery: TStrapiQueryParams = {
+    ...(deepPopulate ? { populate: 'deep' } : {}),
+    locale,
+    ...query,
+  }
+  url.search = qs.stringify(getQuery)
+  return url
+}
