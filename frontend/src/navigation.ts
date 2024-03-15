6735e1351157a81adb1cca7f66fd442ddd49a53c
Ilia Os <dev.osipov@gmail.com>
2024-03-15 17:04:45 +0400

    chore(frontend): always use localePrefix

--- frontend/src/navigation.ts
+++ frontend/src/navigation.ts
@@ -2,7 +2,7 @@ import { createSharedPathnamesNavigation } from 'next-intl/navigation'
 
 export const locales = ['en', 'fr'] as const
 export const defaultLocale = 'en' as const
-export const localePrefix = 'as-needed'
+export const localePrefix: 'as-needed' | 'always' | 'never' = 'always'
 
 export const { Link, redirect, usePathname, useRouter } =
   createSharedPathnamesNavigation({ locales, localePrefix })
