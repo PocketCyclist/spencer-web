ad6db2ca15a76128aaf23b166412540e95438982
Ilia Os <dev.osipov@gmail.com>
2024-03-19 17:40:18 +0400

    feat(frontend): improve buy buttons

--- frontend/src/components/strapi/blocks/ImageCTA/ImageCTA.tsx
+++ frontend/src/components/strapi/blocks/ImageCTA/ImageCTA.tsx
@@ -27,16 +27,18 @@ export const ImageCTA = ({
             href={buyUrl}
             title={buyText}
             className={
-              'relative mx-auto mt-4 flex flex-col items-center justify-center sm:mt-12'
+              'group relative mx-auto mt-4 flex flex-col items-center justify-center transition-all  duration-700 hover:scale-125 sm:mt-12 md:mt-12'
             }
           >
             <BuyCircleIcon
-              className="rotate text-[#FFC441] rem:h-[104px] rem:w-[104px] md:rem:h-[196px] md:rem:w-[196px]"
+              className="rotate text-[#FFC441] transition-colors duration-500 group-hover:text-[#D64100] rem:h-[104px] rem:w-[104px] md:rem:h-[196px] md:rem:w-[196px]"
               height={196}
               width={196}
             />
-            <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center text-[19px] font-extrabold leading-[106%] text-[#D64100] md:rem:text-[36px] md:rem:leading-[38px] ">
-              <div className="max-w-[150px]">{buyText}</div>
+            <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center text-[19px] font-extrabold leading-[106%] text-[#D64100] transition-colors duration-500 group-hover:text-[#FFC441] md:rem:text-[36px] md:rem:leading-[38px]">
+              <div className="rem:max-w-[150px] md:rem:max-w-[150px]">
+                {buyText}
+              </div>
             </div>
           </Link>
         </div>
