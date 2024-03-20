8ff3ac60c9bb7834d86ed8541fc7747b5a9b5aed
Ilia Os <dev.osipov@gmail.com>
2024-03-20 17:40:14 +0400

    feat(frontend): improve LogoHero

--- frontend/src/components/strapi/blocks/LogoHero/LogoHero.tsx
+++ frontend/src/components/strapi/blocks/LogoHero/LogoHero.tsx
@@ -8,7 +8,7 @@ type LogoHeroProps = {
 }
 
 export const LogoHero = ({ bgImage }: LogoHeroProps) => (
-  <section className="relative bg-gray-300 text-white rem:h-[500px] lg:rem:h-[600px] xl:rem:h-[800px]">
+  <section className="relative bg-gray-300 text-white rem:h-[500px] lg:min-h-screen-minus-header">
     <Image
       alt={bgImage.alt}
       className="object-cover object-[70%,100%] lg:object-center"
@@ -17,7 +17,7 @@ export const LogoHero = ({ bgImage }: LogoHeroProps) => (
     />
     <Image
       alt="The Gerard Spencer Project - Handpan performances classes & wellbeing"
-      className="absolute bottom-20 left-1/2 h-auto max-w-[calc(100%-2rem)] -translate-x-1/2 rem:w-[280px] lg:bottom-1/2 lg:translate-y-1/2 lg:rem:w-[400px] xl:rem:w-[740px]"
+      className="absolute bottom-20 left-1/2 h-auto max-w-[calc(100%-2rem)] -translate-x-1/2 rem:w-[280px] lg:bottom-1/2 lg:translate-y-1/2 lg:rem:w-[740px]"
       src="/images/logo-hero.svg"
       width={740}
       height={315}
