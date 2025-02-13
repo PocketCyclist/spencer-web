d3459069c8924b3c927bdc94302036c3dcafee6c
Pavel Hozberg <pavel.hozberg@gmail.com>
2025-02-13 13:32:58 +0300

    Add subscriber

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -2,7 +2,8 @@ import { notFound } from 'next/navigation'
 import { getRequestConfig } from 'next-intl/server'
 import { locales } from '@/navigation'
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound()
 
